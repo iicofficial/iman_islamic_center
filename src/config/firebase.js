31fe66859ed1adc64d7203b1908f6404361982c2
Iman Islamic Center <dev@iman-islam.org>
2026-01-25 23:57:28 -0600

    Security: Move Firebase secrets to environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKSMMRnCsvVwDzt1DiVtr9zXVau9VMf5M",
-    authDomain: "iman-islamic-center.firebaseapp.com",
-    projectId: "iman-islamic-center",
-    storageBucket: "iman-islamic-center.firebasestorage.app",
-    messagingSenderId: "313930017571",
-    appId: "1:313930017571:web:e397ea6674c6636ba8fa96",
-    measurementId: "G-WJYWELMKHS"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
